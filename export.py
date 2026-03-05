9ca8993bda71685ce16ba490f864df61319bac5f
Alexander Belikov <abeliko@sfedu.ru>
2026-03-05 23:57:28 +0300

    adjust donate message

--- export.py
+++ export.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import time
 from stats import record_export
 
 os.makedirs("exported", exist_ok=True)
@@ -30,9 +31,10 @@ def export_playlist(owner, kinds, message, bot):
     bot.send_message(
         message.chat.id,
         "✅ <b>Готово!</b> Если при открытии файла в браузере на мобильных устройствах он отображается неправильно, используйте другое приложение для открытия этого файла.\n\n"
-        "💜 Поддержать (задонатить): https://aleqsanbr.dev\n"
         "📨 Оставить отзыв: /feedback\n\n"
         "Спасибо за использование!",
         parse_mode="HTML"
     )
+    time.sleep(2)
+    bot.send_message(message.chat.id, "Это проект с открытым исходным кодом, который делает и поддерживает один человек. Без рекламы, без подписок — просто и удобно. Если он оказался полезным, небольшое пожертвование поможет ему жить и развиваться.\n\n💜 Поддержать можно тут: https://aleqsanbr.dev\n")
     record_export()
